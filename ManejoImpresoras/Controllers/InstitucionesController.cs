4e09ae5b8b36e268790986aa5dd62d15999fa360
Victor Manuel Yanez Aguilar <victoryanez11@gmail.com>
2023-03-14 20:06:13 -0600

    CRUD para Instituciones (Crear/Sin Depurar)
    Faltan validaciones, etc

---
+++ ManejoImpresoras/Controllers/InstitucionesController.cs
@@ -0,0 +1,46 @@
+﻿using ManejoImpresoras.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoImpresoras.Controllers
+{
+    public class InstitucionesController : Controller
+    {
+        /*
+
+                private readonly string connectionString;
+
+                public InstitucionesController(IConfiguration configuration)
+                {
+                    connectionString = configuration.GetConnectionString("DefaultConnection");
+                }
+        */
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Institucion institucion)
+        {
+            return View();
+        }
+
+        /*
+                public IActionResult Crear()
+                {
+                    using (var connection = new SqlConnection(connectionString))
+
+                    {
+
+                        var query = connection.Query("SELECT 1").FirstOrDefault();
+
+                    }
+                    return View();
+
+                }
+
+         */
+    }
+}
+    
